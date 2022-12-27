045de399f917e57b712eac367037d4008e2a01eb
MypkaXD <85798586+MypkaXD@users.noreply.github.com>
2022-12-27 13:05:02 +0300

    Add files via upload

---
+++ lab4 translyator.cpp
@@ -0,0 +1,22 @@
+﻿#include "Header.h"
+#include <iostream>
+
+int main() {
+
+	std::cout << "ENTER THE EXPRESSION: " << std::endl;
+
+	while (true) {
+		try {
+			char s[1000];
+			std::cin.getline(s, 1000);
+			Lecsems l(s);
+			ArithmeticСalculation ac(l);
+			std::cout << ac.calculating() << std::endl;
+		}
+		catch (const char* e) {
+			std::cout << e << std::endl;
+		}
+	}
+
+	return 0;
+}
